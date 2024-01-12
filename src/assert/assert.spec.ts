08fcdf44a16ad0955908e59027a33eba3fcc410f
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-12 21:54:25 +0900

    feat: assert 함수 테스트 코드 추가

---
+++ src/assert/assert.spec.ts
@@ -0,0 +1,23 @@
+import { assert } from "./assert";
+
+describe("assert", () => {
+  it("condition이 만족되지 않은 경우 에러를 throw해야 합니다.", () => {
+    const value: string = "string";
+
+    expect(() => assert(value === "string1")).toThrowError(Error);
+  });
+
+  it("condition이 만족되지 않은 경우, 인자로 전달된 에러 메시지로 구성된 에러를 throw해야 합니다.", () => {
+    const value = false;
+
+    expect(() => assert(value, "error message")).toThrowError(
+      new Error("error message")
+    );
+  });
+
+  it("condition이 만족된 경우 에러를 throw하지 않아야 합니다.", () => {
+    const value = true;
+
+    expect(() => assert(value)).not.toThrowError();
+  });
+});
