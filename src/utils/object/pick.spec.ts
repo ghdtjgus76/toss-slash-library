b3f2244e0941da72f50117e7f88296cf83b105f5
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-14 22:15:17 +0900

    feat: pick 유틸 함수 테스트 추가

---
+++ src/utils/object/pick.spec.ts
@@ -0,0 +1,25 @@
+import { pick } from "./pick";
+
+export default describe("pick", () => {
+  it("주어진 객체에 있는 키 값으로 이루어진 배열 전달 시 해당 키와 값으로 이루어진 객체를 반환합니다.", () => {
+    const country = {
+      KR: "KR",
+      US: "US",
+      JP: "JP",
+    } as const;
+
+    expect(pick(country, ["JP"])).toStrictEqual({
+      JP: "JP",
+    });
+  });
+
+  it("키 값 배열에 빈 배열 전달 시 실행 결과로 빈 객체를 반환합니다.", () => {
+    const country = {
+      KR: "KR",
+      US: "US",
+      JP: "JP",
+    } as const;
+
+    expect(pick(country, [])).toStrictEqual({});
+  });
+});
