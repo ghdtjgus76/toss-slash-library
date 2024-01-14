b80bdc6eec76dd6cc03334f5d041f584bda1e31a
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-14 23:34:28 +0900

    feat: omit 테스트 코드 추가

---
+++ src/utils/object/omit.spec.ts
@@ -0,0 +1,20 @@
+import { omit } from "./omit";
+
+const country = {
+  KR: "KR",
+  US: "US",
+  JP: "JP",
+} as const;
+
+export default describe("omit 함수", () => {
+  it("빈 배열을 전달할 경우 주어진 객체 그대로를 반환합니다.", () => {
+    expect(omit(country, [])).toStrictEqual(country);
+  });
+
+  it("주어진 객체에 존재하는 키 값으로 이루어진 배열 전달 시 해당 키를 제외한 나머지 키 값으로 이루어진 객체를 반환합니다.", () => {
+    expect(omit(country, ["KR"])).toStrictEqual({
+      US: "US",
+      JP: "JP",
+    });
+  });
+});
