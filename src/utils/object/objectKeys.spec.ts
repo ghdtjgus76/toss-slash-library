ee9f3a6da788d44f343cc058a3696add77baa00e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-15 15:18:02 +0900

    feat: objectKeys 테스트 코드 추가

--- src/utils/object/objectKeys.spec.ts
+++ src/utils/object/objectKeys.spec.ts
@@ -1,13 +1,17 @@
 import { objectKeys } from "./objectKeys";
 
 export default describe("objectKeys", () => {
-  it("objectKeys 함수의 실행 결과는 Object.Keys() 함수의 실행 결과와 동일해야 합니다.", () => {
-    const languages = {
+  let languages: Record<"rust" | "swift" | "js", number>;
+
+  beforeEach(() => {
+    languages = {
       rust: 1,
       swift: 2,
       js: 3,
     } as const;
+  });
 
+  it("objectKeys 함수의 실행 결과는 Object.Keys() 함수의 실행 결과와 동일해야 합니다.", () => {
     expect(objectKeys(languages)).toStrictEqual(Object.keys(languages));
     expect(objectKeys(languages)).toStrictEqual(["rust", "swift", "js"]);
   });
