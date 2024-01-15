03bd1c08157be22fe8dc03e9511d99b26384707d
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-15 15:18:13 +0900

    feat: objectValues 테스트 코드 추가가

--- src/utils/object/objectValues.spec.ts
+++ src/utils/object/objectValues.spec.ts
@@ -1,13 +1,17 @@
 import { objectValues } from "./objectValues";
 
 export default describe("objectValues 함수", () => {
-  it("objectValues 함수의 실행 결과는 Object.values() 함수의 실행 결과와 동일해야 합니다.", () => {
-    const languages = {
+  let languages: Record<"rust" | "swift" | "js", number>;
+
+  beforeEach(() => {
+    languages = {
       rust: 1,
       swift: 2,
       js: 3,
     } as const;
+  });
 
+  it("objectValues 함수의 실행 결과는 Object.values() 함수의 실행 결과와 동일해야 합니다.", () => {
     expect(objectValues(languages)).toStrictEqual(Object.values(languages));
     expect(objectValues(languages)).toStrictEqual([1, 2, 3]);
   });
