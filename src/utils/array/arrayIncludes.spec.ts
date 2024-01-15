cc47d84dd15c5a712ef33e012226527cdeb934e9
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-15 15:21:55 +0900

    refactor: arrayIncludes 테스트 코드 리팩토링

--- src/utils/array/arrayIncludes.spec.ts
+++ src/utils/array/arrayIncludes.spec.ts
@@ -1,22 +1,25 @@
 import { arrayIncludes } from "./arrayIncludes";
 
 export default describe("arrayIncludes 함수", () => {
+  let arr: Array<"a" | "b" | "c">;
+
+  beforeEach(() => {
+    arr = ["a", "b", "c"];
+  });
+
   it("주어진 배열에 있는 요소라면 별다른 타입 선언/단언 없이 true를 반환합니다.", () => {
-    const arr: Array<"a" | "b" | "c"> = ["a", "b", "c"];
     let includedElement = "a";
 
     expect(arrayIncludes(arr, includedElement)).toBe(true);
   });
 
   it("주어진 배열에 없는 요소라면 false를 반환합니다.", () => {
-    const arr: Array<"a" | "b" | "c"> = ["a", "b", "c"];
     let excludedElement = "d";
 
     expect(arrayIncludes(arr, excludedElement)).toBe(false);
   });
 
   it("fromIndex를 입력하면 해당 인덱스 이후부터 주어진 배열에 있는 요소인지 확인 후 결과를 반환합니다.", () => {
-    const arr: Array<"a" | "b" | "c"> = ["a", "b", "c"];
     let element = "a";
 
     expect(arrayIncludes(arr, element, 0)).toBe(true);
