6831969085cc69d190d58884cbd1934951a53b55
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-15 15:20:36 +0900

    refactor: pick 테스트 코드 리팩토링

--- src/utils/object/pick.spec.ts
+++ src/utils/object/pick.spec.ts
@@ -1,12 +1,16 @@
 import { pick } from "./pick";
 
-const country = {
-  KR: "KR",
-  US: "US",
-  JP: "JP",
-} as const;
-
 export default describe("pick", () => {
+  let country: Record<"KR" | "US" | "JP", string>;
+
+  beforeEach(() => {
+    country = {
+      KR: "KR",
+      US: "US",
+      JP: "JP",
+    } as const;
+  });
+
   it("주어진 객체에 있는 키 값으로 이루어진 배열 전달 시 해당 키와 값으로 이루어진 객체를 반환합니다.", () => {
     expect(pick(country, ["JP"])).toStrictEqual({
       JP: "JP",
