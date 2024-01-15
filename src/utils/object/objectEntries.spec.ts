f7672be3b796786a228488a25e2b2dcd12ba1499
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-15 15:15:48 +0900

    feat: objectEntries 테스트 코드 리팩토링링

--- src/utils/object/objectEntries.spec.ts
+++ src/utils/object/objectEntries.spec.ts
@@ -1,13 +1,17 @@
 import { objectEntries } from "./objectEntries";
 
 export default describe("objectEntries 함수", () => {
-  it("objectEntries 함수 실행 결과로 Object.entries() 함수의 실행 결과가 동일해야 합니다.", () => {
-    const languages = {
+  let languages: Record<"rust" | "swift" | "js", number>;
+
+  beforeEach(() => {
+    languages = {
       rust: 1,
       swift: 2,
       js: 3,
     } as const;
+  });
 
+  it("objectEntries 함수 실행 결과로 Object.entries() 함수의 실행 결과가 동일해야 합니다.", () => {
     expect(objectEntries(languages)).toStrictEqual(Object.entries(languages));
     expect(objectEntries(languages)).toStrictEqual([
       ["rust", 1],
